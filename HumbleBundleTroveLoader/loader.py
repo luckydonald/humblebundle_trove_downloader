0d54cf7278cc3da50f0ac935ad352b41f3344c25
luckydonald <mac2019.git@luckydonald.de>
2019-10-09 22:00:05 +0200

    initial working commit.

---
+++ HumbleBundleTroveLoader/loader.py
@@ -0,0 +1,175 @@
+import hashlib
+from typing import List
+
+import requests
+import shutil
+import json
+from os import path
+from bs4 import BeautifulSoup
+from luckydonaldUtils.files.basics import mkdir_p
+
+from settings import DOWNLOAD_DIR, COOKIE_JAR
+
+
+URL_TROVE = "https://www.humblebundle.com/monthly/trove"
+URL_DL_SIGN = "https://www.humblebundle.com/api/v1/user/download/sign"
+URL_DOWNLOAD = "https://dl.humble.com/{file}"
+
+DOWNLOAD_URL_TYPE_TO_SIGNATURE_TYPE_MAP = {
+    'web': 'signed_url',
+    'bittorrent': 'signed_torrent_url',
+}
+
+response = requests.request(
+    method='GET',
+    url=URL_TROVE,
+    cookies=COOKIE_JAR
+)
+
+with open("/tmp/trove.html", "wb") as f:
+    f.write(response.content)
+# end if
+
+soup = BeautifulSoup(response.content)
+json_script_tag_trove_data_element = soup.find('script', id='webpack-monthly-trove-data')
+json_script_tag_trove_data_string = "\n".join(json_script_tag_trove_data_element.contents)
+trove_data = json.loads(json_script_tag_trove_data_string)
+
+GAME_DATA = trove_data['standardProducts']
+
+
+class URLData(object):
+    url: str
+    auth_request: dict
+    file: str
+    type: str
+    size: str
+    md5: str
+    sha1: str
+
+    def __init__(self, url, auth_request, file, type, size, md5, sha1) -> None:
+        self.url = url
+        self.auth_request = auth_request
+        self.file = file
+        self.type = type
+        self.size = size
+        self.md5 = md5
+        self.sha1 = sha1
+        super().__init__()
+    # end if
+# end class
+
+
+DOWNLOADS:List[URLData] = []  # file: url
+for game in GAME_DATA:
+    title = game['machine_name']
+    title = game.get('human_name', title)
+    title = game.get('human-name', title)
+    game_path = path.join(DOWNLOAD_DIR, title)
+    mkdir_p(game_path)
+    for platform, download_meta in game['downloads'].items():
+        download_path = path.join(game_path, platform)
+        mkdir_p(download_path)
+        for download_type, download in download_meta['url'].items():
+            auth_request_data = {
+                "machine_name": download_meta['machine_name'],
+                "filename": download
+            }
+            download_file_path = path.join(download_path, download)
+            for meta_file in ('md5', 'sha1'):
+                if meta_file not in download_meta:
+                    continue
+                # end if
+                meta_file_path = download_path + "." + meta_file
+                with open(meta_file_path, 'w') as f:
+                    f.write(download_meta[meta_file])
+                # end with
+            # end for
+            DOWNLOADS.append(URLData(
+                url=URL_DOWNLOAD.format(file=download),
+                auth_request=auth_request_data,
+                file=download_file_path,
+                type=download_type,
+                size=download_meta['file_size'],
+                md5=download_meta.get('md5', None),
+                sha1=download_meta.get('sha1', None),
+            ))
+        # end for
+    # end for
+    with open(path.join(game_path, 'info.json'), 'w') as f:
+        json.dump(game, f, indent=2, sort_keys=True)
+    # end with
+# end for
+
+url_data: URLData
+for i, url_data in enumerate(DOWNLOADS):
+    print(f'Downloading {url_data.file!r} from {url_data.url!r}')
+    # check if file already exists.
+    if path.exists(url_data.file):
+        print(f'File {url_data.file!r} already exists. Checking size.')
+
+        needs_download = False
+
+        # check file size
+        disk_size = path.getsize(url_data.file)
+        if disk_size != url_data.size:
+            print(f'Existing file {url_data.file!r} has wrong filesize. Disk is {disk_size}, online is {url_data.size}. Will download again.')
+            needs_download = True
+        # end if
+
+        if url_data.type != 'web':
+            print(f'Not checking md5 or sha1 for file {url_data.file!r} as it is not the main file.')
+        else:
+            # check md5 and sha1 hash
+            hash_md5 = hashlib.md5()
+            hash_sha1 = hashlib.sha1()
+            with open(url_data.file, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(chunk)
+                    hash_sha1.update(chunk)
+                # end for
+            # end with
+            md5 = hash_md5.hexdigest()
+            sha1 = hash_sha1.hexdigest()
+            if url_data.md5 is None:
+                print(f'Existing file {url_data.file!r} has no md5 hashsum. Disk is {md5}. Will download again.')
+                needs_download = True
+            elif md5 != url_data.md5:
+                print(f'Existing file {url_data.file!r} has wrong md5 hashsum. Disk is {md5}, online is {url_data.md5}. Will download again.')
+                needs_download = True
+            # end if
+            if url_data.sha1 is None:
+                print(f'Existing file {url_data.file!r} has no sha1 hashsum. Disk is {sha1}. Will download again.')
+                needs_download = True
+            elif sha1 != url_data.sha1:
+                print(f'Existing file {url_data.file!r} has wrong sha1 hashsum. Disk is {sha1}, online is {url_data.sha1}. Will download again.')
+                needs_download = True
+            # end if
+        # end if
+        if not needs_download:
+            print(f'Existing file {url_data.file!r} has correct metadata. Will not download again.')
+            continue
+        # end if
+    # end if
+
+    # get a download url with signature
+    signature = requests.request(
+        method='POST',
+        url=URL_DL_SIGN,
+        headers={
+            'Accept': 'application/json',
+        },
+        cookies=COOKIE_JAR,
+        data=url_data.auth_request,
+    )
+    json_signature = signature.json()
+    url = json_signature[DOWNLOAD_URL_TYPE_TO_SIGNATURE_TYPE_MAP[url_data.type]]
+    print(f'Downloading {url_data.file!r} from signed url {url!r}')
+    with requests.get(url, stream=True) as r:
+        print(f'Downloading {url_data.file!r} from signed url {url!r}: {r}')
+        with open(url_data.file, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        # end with
+    # end with
+# end for
+
---
+++ HumbleBundleTroveLoader/settings.example.py
@@ -0,0 +1,6 @@
+DOWNLOAD_DIR = "/path/to/downloads/HumbleBundleTrove/"
+
+# copy from safari inspector, highlight cookie, press cmd+C:
+COOKIE_JAR = {
+    '_simpleauth_sess': 'kjfhhgjsdkfghsdjfghskjdf all that sweet random stuff from there'
+}
