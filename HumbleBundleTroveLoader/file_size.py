2bcf8a8081ff15eca825459f214b50163c84f8aa
luckydonald <mac2019.git@luckydonald.de>
2019-10-10 05:34:31 +0200

    Fix for size header possibly being `None`.

--- HumbleBundleTroveLoader/file_size.py
+++ HumbleBundleTroveLoader/file_size.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from typing import Union
+
 from luckydonaldUtils.logger import logging
 
 __author__ = 'luckydonald'
@@ -12,13 +14,17 @@ if __name__ == '__main__':
 suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
 
-def human_size(nbytes: int) -> str:
+def human_size(nbytes: Union[int, None]) -> Union[str, None]:
     """
-    https://stackoverflow.com/a/14996816/3423324#python-libraries-to-calculate-human-readable-filesize-from-bytes
+    Calculates a human readable size string.
+    See https://stackoverflow.com/a/14996816/3423324#python-libraries-to-calculate-human-readable-filesize-from-bytes.
 
     :param nbytes: The amount of bytes.
-    :return: a formatted string.
+    :return: a formatted string, or `None` if the input was `None`, too.
     """
+    if nbytes is None:
+        return None
+    # end if
     i = 0
     while nbytes >= 1024 and i < len(suffixes)-1:
         nbytes /= 1024.
