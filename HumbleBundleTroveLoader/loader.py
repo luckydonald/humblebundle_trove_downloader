19016b7cbd522512372ce3137df99edcd96fa800
luckydonald <mac2019.git@luckydonald.de>
2019-10-11 14:12:08 +0200

    Fallback for missing human size.

--- HumbleBundleTroveLoader/loader.py
+++ HumbleBundleTroveLoader/loader.py
@@ -84,7 +84,7 @@ for i, game_data in enumerate(GAME_DATA):
                 md5=dl['md5'],
                 sha1=dl.get('sha1'),
                 file_size=dl['file_size'],
-                human_size=dl['size'],
+                human_size=dl.get('size', human_size(dl['file_size'])),
                 uploaded_at=dl.get('uploaded_at'),
             )
             for k, dl in game_data['downloads'].items()
